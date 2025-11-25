4fbb8d324b90400a9115768263c54c7e0c834ae9
AngelMedinaSo <9030149@student.tcrmbo.nl>
2025-11-25 10:21:35 +0100

    bonus punten

--- opdracht_1/script.js
+++ opdracht_1/script.js
@@ -1,2 +1,5 @@
 document.getElementById("titel").style.color = "red";
 document.getElementById("titel").innerHTML = "doei";
+
+document.getElementById("titel2").style.color = "red";
+document.getElementById("titel2").innerHTML = "doei";

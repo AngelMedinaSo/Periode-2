cbe91112349cda860e8e84e730f65e71191d5ce8
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-01-20 09:45:44 +0100

    a

---
+++ Opdracht 13 - Leeftijdcheck/index.js
@@ -0,0 +1,9 @@
+let leeftijd = 17;
+
+if (leeftijd >= 18) {
+  document.getElementById("titel").innerHTML =
+    "Je mag hier nog niet naar binnen.";
+} else {
+  document.getElementById("titel").innerHTML =
+    "Welkon!";
+}

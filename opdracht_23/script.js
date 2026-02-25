7debf21cc204af2b148853ce2688235e06c4fe6f
AngelMedinaSo <9030149@student.tcrmbo.nl>
2026-02-26 00:25:12 +0100

    opdracht 24

--- opdracht_23/script.js
+++ opdracht_23/script.js
@@ -1,12 +1,9 @@
-let emojis = ["😆","😎","💀","😘","🤩"];
+let emojis = ["😆","😎","😉","😘","🤩"];
 let index = 0;
 const e = document.getElementById("emoji");
 
 function showEmoji() {
     e.innerText = emojis[index];
-    const colors = ["red", "blue", "green", "orange", "purple"];
-    e.style.color = colors[Math.floor(Math.random() * colors.length)];
-    e.style.fontSize = `${50 + Math.floor(Math.random() * 20)}px`;
     index++;
     if (index >= emojis.length) {
         index = 0;
